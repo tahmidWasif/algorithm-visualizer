fe7b2171af0da409bf1055a282af5227108e6f35
root <root@LAPTOP-KIARFKCV.localdomain>
2025-12-02 00:14:43 -0600

    video playback fixed

--- src/main/java/com/visualizer/algorithmvisualizer/Main.java
+++ src/main/java/com/visualizer/algorithmvisualizer/Main.java
@@ -16,22 +16,23 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        // Load video
         Media media = new Media(new File("background.mp4").toURI().toString());
         MediaPlayer player = new MediaPlayer(media);
         player.setCycleCount(MediaPlayer.INDEFINITE); // loop forever
         player.setAutoPlay(true);
 
         MediaView mediaView = new MediaView(player);
-        mediaView.setPreserveRatio(false); // stretch to fill
-        mediaView.setFitWidth(1280);
-        mediaView.setFitHeight(720);
+        mediaView.setPreserveRatio(false);
 
         StackPane root = new StackPane();
 
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
         Parent ui = fxmlLoader.load();
 
-        root.getChildren().addAll(ui);
+        // Add MediaView first (background), then UI
+        root.getChildren().addAll(mediaView, ui);
+
         Scene scene = new Scene(root, 1280, 720);
         stage.setTitle("Algorithm Visualizer");
         stage.setScene(scene);
@@ -39,8 +40,8 @@ public class Main extends Application {
 
         player.setOnError(() -> System.out.println("Error: " + player.getError()));
         media.setOnError(() -> System.out.println("Media error: " + media.getError()));
-
     }
+
 
     public static void main(String[] args) {
         launch();
