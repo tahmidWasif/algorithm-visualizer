2d7bdaf29ea51b0247202a4b07738165e2796bf1
Md Tahmid Wasif <mxw9311@mavs.uta.edu>
2025-12-01 23:30:43 -0600

    designing main view

--- src/main/java/com/visualizer/algorithmvisualizer/Main.java
+++ src/main/java/com/visualizer/algorithmvisualizer/Main.java
@@ -2,19 +2,44 @@ package com.visualizer.algorithmvisualizer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Media media = new Media(new File("background.mp4").toURI().toString());
+        MediaPlayer player = new MediaPlayer(media);
+        player.setCycleCount(MediaPlayer.INDEFINITE); // loop forever
+        player.setAutoPlay(true);
+
+        MediaView mediaView = new MediaView(player);
+        mediaView.setPreserveRatio(false); // stretch to fill
+        mediaView.setFitWidth(1280);
+        mediaView.setFitHeight(720);
+
+        StackPane root = new StackPane();
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Parent ui = fxmlLoader.load();
+
+        root.getChildren().addAll(ui);
+        Scene scene = new Scene(root, 1280, 720);
         stage.setTitle("Algorithm Visualizer");
         stage.setScene(scene);
         stage.show();
+
+        player.setOnError(() -> System.out.println("Error: " + player.getError()));
+        media.setOnError(() -> System.out.println("Media error: " + media.getError()));
+
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.visualizer.algorithmvisualizer {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires javafx.media;
 
 
     opens com.visualizer.algorithmvisualizer to javafx.fxml;
